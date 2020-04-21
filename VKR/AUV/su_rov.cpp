e8f555ecd3364a840f3195a7d7b5acbec824476d
Надир Надирбеков <nadirbekovnadir@yandex.ru>
2020-04-21 15:08:13 +0300

    costil

---
+++ VKR/AUV/su_rov.cpp
@@ -0,0 +1,219 @@
+#include "AUV/su_rov.h"
+#include <QtMath>
+
+double X[2000][2];
+
+SU_ROV::SU_ROV(QObject *parent) : QObject(parent)
+{
+    K_Protocol = new Qkx_coeffs("protocols.conf", "ki");
+    X_Protocol = new x_protocol ("protocols.conf","xi",X);
+    model = new ROV_Model ();
+
+    X[41][0] = 0; // Начальное значение курса равно нулю
+    X[9][0] = 0;
+
+    //if (K[1]>0) T=K[1];
+    /*else*/ T=0.01;
+    time.start(T*1000);//запуск проводим в мсек
+    connect (&time, SIGNAL(timeout()), SLOT(tick()));
+}
+
+
+
+void SU_ROV::tick()
+{
+    get_data_from_model();
+
+    if (isFollowMod)
+        CourseOnPoint();
+    Control_Kurs();
+    Control_Marsh();
+    BFS_DRK(X[49][0], 0, X[9][0],0);
+    //BFS_DRK(0, 1, 0, 0);
+
+    model->tick(X[22][0], X[23][0], X[24][0], X[25][0], T);
+
+    emit DataChanged();
+}
+
+
+
+
+
+void SU_ROV::addKurs()
+{
+    qDebug() << "Course before changes: " << X[41][0];
+    X[41][0]++;
+    qDebug() << "Course after changes: " << X[41][0];
+}
+void SU_ROV::decKurs(){
+    X[41][0]--;
+}
+
+void SU_ROV::setKurs(double angle)
+{
+    X[41][0] = angle;
+}
+
+void SU_ROV::setMarshSpeed(double value)
+{
+    X[9][0] = value;
+}
+
+void SU_ROV::changeMode(int btn, bool state)
+{
+    if (btn==Automatiz && state) mode=Automatiz;
+    else mode=Ruchnoi;
+}
+
+void SU_ROV::setInitialPosition(double x, double z)
+{
+    //X[35][0] = x;
+    //X[37][0] = z;
+}
+
+void SU_ROV::setFollowPoint(double x, double z)
+{
+    followX = x;
+    followZ = z;
+}
+
+void SU_ROV::enableFollowMod()
+{
+    isFollowMod = true;
+}
+
+void SU_ROV::disableFollowMod()
+{
+    isFollowMod = false;
+}
+
+float SU_ROV::getDeltaT()
+{
+    return T;
+}
+
+
+
+void SU_ROV::BFS_DRK(double Upsi, double Uteta, double Ux, double Uy){
+
+    //ограничим входные задающие сигналы в бфс ДРК
+    X[11][0] = saturation(Ux, K[11]);
+    X[12][0] = saturation(Upsi,K[12]);
+    X[13][0] = saturation(Uteta,K[13]);
+    X[74][0] = saturation(Uy, K[66]);
+
+    //далее по структурной схеме БФС, вычисляем значения после первого сумматора
+    X[14][0] = -X[12][0] + X[11][0]; //промежуточное значение для ВМА МЛ (управление курсом и маршем)
+    X[15][0] = X[12][0] + X[11][0]; //промежуточное значение для ВМА МПр (управление курсом и маршем)
+    X[16][0] = -X[74][0] - X[13][0]; //промежуточное значение для ВМА ВК (управление дифферентом и глубиной)
+    X[17][0] = -X[74][0] + X[13][0]; //промежуточное значение для ВМА ВН (управление дифферентом и глубиной)
+
+
+    //ограничим и промасштабируем управляющие значения напряжений для ВМА
+    X[18][0] = saturation(X[14][0],K[14]); //управляющее напряжение на ВМА МВЛ
+    X[19][0] = saturation(X[15][0],K[15]); //управляющее напряжение на ВМА МНЛ
+    X[20][0] = saturation(X[16][0],K[16]); //управляющее напряжение на ВМА МВП
+    X[21][0] = saturation(X[17][0],K[17]); //управляющее напряжение на ВМА МНП
+
+    X[22][0] = X[18][0]*K[22];
+    X[23][0] = X[19][0]*K[23];
+    X[24][0] = X[20][0]*K[24];
+    X[25][0] = X[21][0]*K[25];
+}
+
+void SU_ROV::Control_Kurs()
+{
+
+    //X[41][0]=K[20]; //INFO: заданный курс для отладки через kx-pult
+    X[43][0]=X[41][0]-X[42][0];
+    X[44][0]=X[43][0]*K[44];
+
+    X[45][0]=X[50][0]*K[45];
+    X[46][0]=X[44][0]-X[45][0]+X[41][0]*K[43];
+    X[48][0]=X[46][0];
+    X[49][0]=saturation(X[48][0],K[49]);
+
+
+}
+
+void SU_ROV::Control_Marsh()
+{
+    //X[9][0]=K[];
+}
+
+void SU_ROV::Control_Different(){
+    //mode = Automatiz; //NOTE: отладка
+
+
+    X[90][0]=K[18]; //заданное значение дифферента
+    X[91][0]=X[90][0];
+    X[93][0]=X[91][0]-X[39][0];
+    X[94][0]=X[93][0]*K[90];
+
+    X[100][0]=X[91][0]*K[91];
+    X[108][0] = sin(X[39][0]/57.3)*K[108];
+    X[101][0]=X[94][0]+X[100][0]+X[108][0];
+
+    X[102][0]=X[51][0]*K[93];
+    X[103][0]=X[101][0]-X[102][0];
+    X[104][0]=saturation(X[103][0],K[95]);
+}
+
+void SU_ROV::Control_Depth()
+{
+    X[60][0]=K[19]; //NOTE: заданная глубина через коэффициент для тестирования
+    X[61][0]=X[60][0]-X[62][0];
+    X[63][0]=K[60]*X[61][0];
+
+    X[68][0]=X[73][0]*K[64];
+    X[69][0]=X[63][0]+K[65]*X[60][0];
+    X[77][0]=X[69][0]-X[68][0];
+    6X[70][0]=saturation(X[77][0],K[63]);
+}
+
+void SU_ROV::CourseOnPoint()
+{
+    double deltaX = followX - X[35][0];
+    double deltaZ = followZ - X[37][0];
+
+    if (deltaZ < __DBL_EPSILON__)
+    {
+        disableFollowMod();
+        return;
+    }
+
+    if (qPow(deltaX, 2) + qPow(deltaZ, 2) < 0.01)
+    {
+        disableFollowMod();
+        return;
+    }
+
+    X[41][0] = qAtan(deltaZ / deltaX) / M_PI * 180;
+    if (deltaZ < 0)
+        X[41][0] *= -1;
+}
+
+float SU_ROV::saturation(float input, float max) {
+    if (fabs(input)>= max)
+        return (sign(input)*max);
+    else return input;
+}
+
+
+void SU_ROV::get_data_from_model()
+{
+    X[32][0]=model->Fx;
+    X[33][0]=model->vx_global;
+    X[34][0]=model->vx_local;
+    X[35][0]=model->x_global;
+    X[36][0]=model->y_global;
+    X[37][0]=model->z_global;
+    X[39][0]=model->Tetta_g;
+    X[40][0]=model->Gamma_g;
+    X[51][0]=model->W_Tetta_g;
+    X[42][0]=model->Psi_g;
+    X[50][0]=model->W_Psi_g;
+    X[73][0]=model->vy_global;
+    X[62][0]=model->cur_depth;
+}
---
+++ VKR/AUV/vehicle.cpp
@@ -0,0 +1,140 @@
+#include "AUV/vehicle.h"
+#include <QPoint>
+#include <QRect>
+#include <QPainterPath>
+#include <QPainter>
+#include <QPen>
+#include <QtMath>
+#include "map.h"
+#include <QList>
+#include <QGraphicsItem>
+#include "obstacle.h"
+
+void Vehicle::SetCoordinates(double x, double y)
+{
+    this->setPos(x, y);
+    coordinates.x = x;
+    coordinates.y = y; //why?
+
+    ROV->setInitialPosition(x, y);
+}
+
+void Vehicle::UpdateSonarData()
+{
+    if (map == nullptr)
+        map = this->scene();
+
+    QList<QGraphicsItem *>items;
+
+    double tempCos;
+    double tempSin;
+    double searchAngle;
+
+    double tempX;
+    double tempY;
+    bool obstacleDetected = false;
+
+    searchAngle = courseAngle - sonar_viewAngle/2;
+
+    for (int i = 0; i < sonar_numberOfBeams; i++)
+    {
+        obstacleDetected = false;
+
+        sonar_data[i] = sonar_range;
+        searchAngle += sonar_viewAngle / sonar_numberOfBeams;
+        tempCos = qCos(searchAngle / 180 * M_PI);
+        tempSin = qSin(searchAngle / 180 * M_PI);
+
+        tempX = coordinates.x;
+        tempY = coordinates.y;
+
+        for (double length = 0; length <= sonar_range; length += sonar_rangeStep)
+        {
+            tempX += sonar_rangeStep * tempCos;
+            tempY += sonar_rangeStep * tempSin;
+
+            items = map->items(QPointF(tempX, tempY));
+
+            for (auto iter = items.begin(); iter != items.end(); iter++)
+            {
+                if ((*iter)->type() == Obstacle::Type)
+                {
+                    obstacleDetected = true;
+                    break;
+                }
+
+            }
+
+            if (obstacleDetected)
+            {
+                sonar_data[i] = length;
+                break;
+            }
+        }
+    }
+}
+
+void Vehicle::SetCourseAngle(double angle)
+{
+    ROV->setKurs(angle);
+}
+
+void Vehicle::MoveForward(double speed)
+{
+    ROV->setMarshSpeed(speed);
+}
+
+void Vehicle::MoveToPoint(double x, double y)
+{
+    ROV->setFollowPoint(x, y);
+    ROV->enableFollowMod();
+    ROV->setMarshSpeed(100);
+}
+
+void Vehicle::UpdateDataFromROV()
+{
+    this->setRotation(X[42][0]);
+
+    this->setX(X[35][0]);
+    this->setY(X[37][0]);
+}
+
+Vehicle::Vehicle()
+{
+    setFlag(ItemIsMovable);
+
+    form << QPoint(15, 0)
+         << QPoint(-15, -10)
+         << QPoint(-15, 10);
+
+    ROV = new SU_ROV();
+    connect(ROV, SIGNAL(DataChanged()), (QObject*)this, SLOT(UpdateDataFromROV()));
+}
+
+Vehicle::~Vehicle()
+{
+
+}
+
+QRectF Vehicle::boundingRect() const
+{
+    QRect rect(-15, -10, 30, 20);
+    return rect;
+}
+
+QPainterPath Vehicle::shape() const
+{
+    QPainterPath path;
+    path.addPolygon(form);
+    return path;
+}
+
+void Vehicle::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(widget)
+    Q_UNUSED(option)
+
+    painter->setPen(QPen(Qt::darkGreen));
+    painter->setBrush(Qt::green);
+    painter->drawPolygon(form);
+}
